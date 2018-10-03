98907c3605ee1c5cf398f4ebf9915fb805c178c2
Matthew Russell <mattrussell36@gmail.com>
2018-10-03 23:14:38 +0100

    Change card to be horizontal layout

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,23 +2,35 @@ import * as React from 'react';
 import styled from 'styled-components';
 
 interface IProps {
-  title: string,
-  children: any
+  title: string;
+  img: string;
+  children: any;
 }
 
 const CardDiv = styled.div`
-  padding: 20px;
-  min-height: 100%;
+  display: flex;
+  align-items: flex-start;
   background-color: ${props => props.theme.colors.cardBackground}; 
   box-shadow: 1px 2px 4px 0px rgba(80, 81, 82, 0.2);
 `;
 
-const Card: React.SFC<IProps> = ({ title, children}) => (
+const CardImg = styled.img`
+  display: block;
+  width: 130px;
+  height: auto;
+`;
+
+const CardContent = styled.div`
+  padding: 20px;
+`;
+
+const Card: React.SFC<IProps> = ({ title, img, children}) => (
   <CardDiv>
-    <h3 style={{ marginTop: 0 }}>{title}</h3>
-    <div>
+    <CardImg src={img}  alt={title} />
+    <CardContent>
+      <h3 style={{ marginTop: 0 }}>{title}</h3>
       {children}
-    </div>
+    </CardContent>
   </CardDiv>
 );
 
