6901312ef0dadc0c318c3aacc62db0356da6d6e9
Matthew Russell <mattrussell36@gmail.com>
2018-10-03 23:15:57 +0100

    mauerwerk react grid component

--- src/components/MovieList/index.tsx
+++ src/components/MovieList/index.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import { IMovie } from '../../store/movies/types';
 import Card from '../Card';
+
+import { Grid } from 'mauerwerk';
+import styled from 'styled-components';
 
 interface IProps {
   movies: IMovie[]
 }
 
-const Grid = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-`;
-
-const GridItem = styled.li`
-  padding: 10px;
+const Pill = styled.li`
+  display: inline-block;
+  margin: 0 5px 5px 0;
+  padding: 5px 10px;
+  border-radius: 8px;
+  background-color: white;
 `;
 
 const MovieList: React.SFC<IProps> = ({ movies }) => {
@@ -22,16 +23,23 @@ const MovieList: React.SFC<IProps> = ({ movies }) => {
   }
 
   return (
-    <Grid>
-      {movies.map((movie: IMovie) => (
-          <GridItem key={movie.id}>
-            <Card title={movie.title}>
-              <p>Genre IDs: {movie.genres.map(genre => genre.name).join(', ')}</p>
-              <p>Popularity: {movie.popularity}</p>
-              <p style={{ marginBottom: 0 }}>Rating: {movie.vote_average}</p>
-            </Card>
-          </GridItem>
-      ))}
+    <Grid
+      data={movies}
+      keys={(d: IMovie) => d.id}
+      heights={(d: IMovie) => 195}
+      columns={2}
+      margin={20}
+      transitionMount={true}
+    >
+      {(movie: IMovie) => (
+        <Card title={movie.title} img={'https://image.tmdb.org/t/p/w500/' + movie.poster_path}>
+          <ul>
+            {movie.genres.map(genre => <Pill key={genre.id}>{genre.name}</Pill>)}
+          </ul>
+          <p><strong>Popularity:</strong> {movie.popularity}</p>
+          <p style={{ marginBottom: 0 }}><strong>Rating:</strong> {movie.vote_average}</p>
+        </Card>
+      )}
     </Grid>
   )
 }
